acc552ab771db16c7d1ae153d6a72cc15b56695a
Jorge Arreola <jorgitoarreolas@gmail.com>
2021-06-18 00:55:40 -0500

    Setup script enhancer

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 setLink (){
     file=$1
-    binPath=$( [[ ! -z "$2" ]] && echo "$2" || echo "/bin/${1/#.\/}" )
+    chmod +x $file
+
+    binPath=$( [[ ! -z "$2" ]] && echo "$2" || echo "/bin/${1##*/}" )
 
     if [ -f "$binPath" ]; then
         echo "Existing link in $binPath, deleting..."
@@ -10,10 +12,12 @@ setLink (){
     fi
 
     echo "Creating link of: $file -> $binPath"
-    sudo ln -sbf $file $binPath
+    sudo ln -f $file $binPath
 }
 
-setLink ./mkdcd
-setLink ./removeNvimSwap
+for file in $( find ./scripts -type f ); do
+    echo "Found: $file"
+    setLink $file
+done
 
 alias n=nvim 
