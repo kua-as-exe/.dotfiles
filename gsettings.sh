3e40e33e5e754ab1715d252fda86b0a56a1a6853
Jorge Arreola <jorgitoarreolas@gmail.com>
2021-06-30 03:32:38 -0500

    gsettings

---
+++ gsettings.sh
@@ -0,0 +1,10 @@
+
+
+# Hide mounted devices in dock
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
+
+# Isolated workspaces
+gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
+
+# Menu button on left side
+gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
