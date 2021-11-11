6efc1a5ab537fa7ec6995d6bc85e24bb45b291d5
Jorge Arreola <jorgitoarreolas@gmail.com>
2021-11-11 14:50:56 -0600

    Codi save option

--- scripts/codi.sh
+++ scripts/codi.sh
@@ -7,7 +7,7 @@ syntax="${1:-python}"
 shift
 vim -c \
   "let g:startify_disable_at_vimenter = 1 |\
-  set bt=nofile ls=0 noru nonu nornu nocursorline |\
+  set ls=0 noru nonu nornu nocursorline |\
   hi ColorColumn ctermbg=NONE |\
   hi VertSplit ctermbg=NONE |\
   hi NonText ctermfg=0 |\
