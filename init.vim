573f8223328eed23e6c2b42726ddbea05fd2eca9
Jorge Arreola <jorgitoarreolas@gmail.com>
2021-12-06 16:32:50 -0600

    Enhances in setup

--- init.vim
+++ init.vim
@@ -2,6 +2,14 @@
 " Personal Nvim Configuration
 " ~ Jorge Arreola
 "
+
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+if empty(glob(data_dir . '/autoload/plug.vim'))
+  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+
+
 call plug#begin('~/.local/share/nvim/plugged')
 
 " THEME
--- setup.sh
+++ setup.sh
@@ -2,13 +2,19 @@
 
 # cat instalations.sh | sed -e "s/apt install/apt install -y/" | bash
 
+NVIM_CONFIG_FOLDER="$HOME/.config/nvim/"
+
+[ ! -f "./bin" ] && mkdir -p "./bin"
+mkdir ./.local
+[ ! -f "$NVIM_CONFIG_FOLDER" ] && mkdir -p "$NVIM_CONFIG_FOLDER"
+
 setLink (){
     file=$1
     chmod +x $file
 
     filename="@${1##*/}"
     filename=$( basename $filename .sh )
-    binPath=$( [[ ! -z "$2" ]] && echo "$2" || echo "/bin/$filename" )
+    binPath=$( [[ ! -z "$2" ]] && echo "$2" || echo "./bin/$filename" )
 
     if [ -f "$binPath" ]; then
         echo "Existing link in $binPath, deleting..."
@@ -27,6 +33,5 @@ done
 
 alias n=nvim 
 alias rm=trash
-ln -f ./init.vim ~/.config/nvim/init.vim
+ln -f "./init.vim" "$NVIM_CONFIG_FOLDER/init.vim"
 
-mkdir ./.local
